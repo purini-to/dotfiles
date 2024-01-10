98b0a125f206581ae793a4b294cea0bf3eb60944
purini-to <biz.hr.urainkyo@gmail.com>
2024-01-10 10:39:17 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,22 +4,31 @@ brew cask install visual-studio-code
 brew install neovim
 brew cask install google-chrome
 # brew cask install alacritty
-brew cask install iterm2
-brew cask install docker
-brew install docker
+brew install --cask docker
 brew cask install slack
 brew cask install clipy
-brew install fish
 brew install fzf
 brew install ghq
-brew install bat
 brew install ripgrep
 brew install hub
 brew install gibo
 brew install jq
-brew install vegeta
-brew install rs/tap/jaggr
-brew install rs/tap/jplot
+
+# ----- terminal -----
+brew cask install iterm2
+brew install fish
+brew install starship
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+
+starship init fish | source
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
+fisher add 0rax/fish-bd
+fisher add decors/fish-ghq
+fisher add jethrokuan/fzf
+fisher add jethrokuan/z
+fisher add matchai/spacefish
 
 # ----- kubernetes -----
 # brew install kubectx
@@ -31,12 +40,6 @@ brew install rs/tap/jplot
 # ----- gRPC -----
 # brew install protobuf
 # ----------------
-
-fisher add 0rax/fish-bd
-fisher add decors/fish-ghq
-fisher add jethrokuan/fzf
-fisher add jethrokuan/z
-fisher add matchai/spacefish
 
 git config --system alias.s status
 git config --system alias.d diff
