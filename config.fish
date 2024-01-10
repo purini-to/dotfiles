b345918ef0980807a6651b095acdd7b0aca8c3dd
purini-to <biz.hr.urainkyo@gmail.com>
2024-01-10 10:41:51 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,10 +5,10 @@ set -U fish_user_paths $GOPATH/bin $fish_user_paths
 
 set -x DOCKER_BUILDKIT 1
 
-set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
+# set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
 
-set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
-eval (pyenv init - | source)
+# set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
+# eval (pyenv init - | source)
 
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
@@ -23,8 +23,8 @@ alias g git
 alias d docker
 alias dc docker-compose
 
-alias vi nvim
-alias vim nvim
+# alias vi nvim
+# alias vim nvim
 
 alias gs "git status"
 alias gc "git commit"
@@ -38,8 +38,9 @@ alias greset "git clean -fd && git reset --hard && git status"
 
 alias see "hub browse"
 
-alias k "kubectl"
-alias kg "kubectl get"
-alias kfar "kubectl api-resources | grep"
-alias kfav "kubectl api-versions  | grep"
+# alias k "kubectl"
+# alias kg "kubectl get"
+# alias kfar "kubectl api-resources | grep"
+# alias kfav "kubectl api-versions  | grep"
 
+starship init fish | source
